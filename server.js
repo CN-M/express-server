23d05f94142094f35ff0e9d65d7d55de70b0581c
C.N. <cn.mbhalati@gmail.com>
2024-03-26 21:19:53 +0200

    Add error handling, forwarding of 404 errors, edit main GET endpoint

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const createError = require("http-errors");
 require("dotenv").config();
 require("colors");
 
@@ -8,8 +9,33 @@ const port = PORT || 8000;
 const app = express();
 
 app.get("/", (req, res) => {
-  res.status = 200;
-  res.json({ message: "Hello world" });
+  const { name } = req.query;
+
+  if (name) {
+    res.status(200).json({ message: `Hello, ${name}!` });
+  } else {
+    res.status(200).json({ message: "Hello world" });
+  }
+});
+
+// Catch 404 errors and forward to error handler
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+// Error Handler
+app.use((err, req, res, next) => {
+  // Set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // Send JSON response for errors
+  res.status(err.status || 500).json({
+    error: {
+      status: err.status || 500,
+      message: err.message || "Internal Server Error",
+    },
+  });
 });
 
 app.listen(port, () => {
